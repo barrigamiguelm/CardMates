96054b05436c5f0b790b3f8612437f644a79e55d
Rafael Librero <librerorafael@outlook.com>
2022-10-25 18:48:16 +0200

    Bottom Navigation funcionando

--- app/src/main/java/com/example/cardmates/MainActivity.java
+++ app/src/main/java/com/example/cardmates/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cardmates;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,11 +12,14 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.cardmates.Activities.Login;
+import com.example.cardmates.databinding.ActivityMainBinding;
 import com.google.common.collect.Maps;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     private Button btnLogOutMain;
 
@@ -21,9 +27,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        btnLogOutMain = (Button) findViewById(R.id.btnLogOutMain);
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.chat:
+                    replaceFragment(new ChatFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+            }
+
+            return true;
+        });
+
+        /*btnLogOutMain = (Button) findViewById(R.id.btnLogOutMain);
+
         mAuth =FirebaseAuth.getInstance();
 
 
@@ -33,10 +59,19 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(MainActivity.this, Login.class));
             }
-        });
-
+        });*/
 
     }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+    }
+
+
 
 
 
