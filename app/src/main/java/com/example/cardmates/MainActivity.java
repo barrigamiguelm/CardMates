9759d81d7ecab929449e60cd12f813f180bd929d
Miguel <barrigamiguelm@gmail.com>
2022-10-28 13:10:28 +0200

    "API de google maps y dependencias"

--- app/src/main/java/com/example/cardmates/MainActivity.java
+++ app/src/main/java/com/example/cardmates/MainActivity.java
@@ -6,26 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.cardmates.Activities.Login;
-import com.google.common.collect.Maps;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogOutMain;
-
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnLogOutMain = (Button) findViewById(R.id.btnLogOutMain);
-        mAuth =FirebaseAuth.getInstance();
-
+        mAuth = FirebaseAuth.getInstance();
 
         btnLogOutMain.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,10 +33,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
-
-
-
 
 }
