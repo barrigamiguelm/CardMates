be478bea0addeeb9ca3cdd67be7ba15f75066c74
Miguel <barrigamiguelm@gmail.com>
2022-10-31 10:07:26 +0100

    "Fixed un par de lineas para poder hacer Merge"

--- app/src/main/java/com/example/cardmates/MainActivity.java
+++ app/src/main/java/com/example/cardmates/MainActivity.java
@@ -5,17 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.example.cardmates.Activities.Login;
 import com.example.cardmates.databinding.ActivityMainBinding;
-import com.google.common.collect.Maps;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLogOutMain;
 
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
 
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
 
                 case R.id.home:
                     replaceFragment(new HomeFragment());
@@ -48,34 +43,16 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        /*btnLogOutMain = (Button) findViewById(R.id.btnLogOutMain);
-
-        mAuth =FirebaseAuth.getInstance();
-
-
-        btnLogOutMain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, Login.class));
-            }
-        });*/
 
     }
 
-    private void replaceFragment(Fragment fragment){
+    private void replaceFragment(Fragment fragment) {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
     }
-
-
-
-
-
-
 
 
 }
