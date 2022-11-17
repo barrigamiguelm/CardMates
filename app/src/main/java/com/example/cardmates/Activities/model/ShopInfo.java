3af207290df3412893cd6005dad5437b65a79ec9
Miguel Barriga <miguel.barriga@radmas.com>
2022-11-17 21:21:31 +0100

    "Cambiado colo fondo userlist, framelayout y clase que faltaba"

---
+++ app/src/main/java/com/example/cardmates/Activities/model/ShopInfo.java
@@ -0,0 +1,60 @@
+package com.example.cardmates.Activities.model;
+
+public class ShopInfo {
+
+    String name,desc,img;
+    Double lat, lang;
+
+
+
+    public ShopInfo(String name, String desc, Double lat, Double lang) {
+        this.name = name;
+        this.desc = desc;
+        this.lat = lat;
+        this.lang = lang;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public ShopInfo() {
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLang() {
+        return lang;
+    }
+
+    public void setLang(Double lang) {
+        this.lang = lang;
+    }
+}
