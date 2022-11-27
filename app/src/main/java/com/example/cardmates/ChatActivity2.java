fc08b9dcce379f28fbf7ed03689d6f1ffd37ce86
Rafael Librero <librerorafael@outlook.com>
2022-11-27 20:56:27 +0100

    Boton imagenback del chat permite ir hacia atras

--- app/src/main/java/com/example/cardmates/ChatActivity2.java
+++ app/src/main/java/com/example/cardmates/ChatActivity2.java
@@ -158,15 +158,13 @@ public class ChatActivity2 extends AppCompatActivity {
 
     public void setListeners() {
         binding.layoutSend.setOnClickListener(v -> sendMessage());
-        binding.imageMaps.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (view.getId()==R.id.imageMaps){
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer, new MapsFragment()).commit();
-                    binding.chatActivity.setVisibility(View.GONE);
-                }
+        binding.imageMaps.setOnClickListener(view -> {
+            if (view.getId()==R.id.imageMaps){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentcontainer, new MapsFragment()).commit();
+                binding.chatActivity.setVisibility(View.GONE);
             }
         });
+        binding.imageBack.setOnClickListener(view -> onBackPressed());
     }
 
     private String getReadableDateTime(Date date) {
--- app/src/main/java/com/example/cardmates/MapsFragment.java
+++ app/src/main/java/com/example/cardmates/MapsFragment.java
@@ -27,7 +27,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MapsFragment extends Fragment {
-
+    //TODO: Valorar si maps debe de ser un fragment o una activity
     GoogleMap mMap;
 
 
